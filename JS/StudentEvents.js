0afa02b6d721e4903a85e8f247b08d1514995ee1
TerryDavisDevine <oneill-f27@ulster.ac.uk>
2026-04-11 20:05:48 +0100

    displaying events

--- JS/StudentEvents.js
+++ JS/StudentEvents.js
@@ -1,7 +1,7 @@
 //get the events from the json file
 async function getEvents(){
   try{
-    let response = await fetch("../resources/events.json");
+    let response = await fetch("../Resources/json/events.json");
     let text = await response.text();
     let data = JSON.parse(text);
     return data;
@@ -10,30 +10,41 @@ async function getEvents(){
   }
 }
 //create the inidivudal elemtns for the events
-function createElement(type,content){
-  let node = document.createElement(type);
-  node.innerHTML=content;
-  return node
+function createElement(type,content,attrName="",attrCon=""){
+  if(type!="img"){
+    let node = document.createElement(type);
+    node.innerHTML=content;
+    if(attrName!=""){
+      node.setAttribute(attrName,attrCon);
+    }else{
+      node.setAttribute("class","eventText");
+    }
+    return node
+  }else{
+    let node = document.createElement(type);
+    node.setAttribute("src",content);
+    return node
+  }
 }
 //create the elemtns for the events
 function createEvent(details){
   let div = document.createElement("div");
   div.setAttribute("class","event");
-  div.appendChild(createElement("h3",details.Name));
+  let img = createElement("img",details.Img);
+  img.setAttribute("class","eventImage");
+  div.appendChild(img);
+  div.appendChild(createElement("h3",`${details.Name} - ${details.Location}`,"class","eventText eventTextTitle"));
   div.appendChild(createElement("h4",details.Date));
   div.appendChild(createElement("h4",details.Time));
-  div.appendChild(createElement("h4",details.Location));
   div.appendChild(createElement("h4",details.Desc));
   return div;
 }
 //add elements to the page
 function showEvents(events){
     let allEvents = events.events;
-    console.log(allEvents.length);
     let eventSpace = document.getElementById("eventSpace");
     for(let i =0; i<allEvents.length;i++){
-      console.log("HI");
-      eventSpace.appendChild(createEvent(allEvents[0]));
+      eventSpace.appendChild(createEvent(allEvents[i]));
    }
 }
 //waits for getEvents to actually give the data
